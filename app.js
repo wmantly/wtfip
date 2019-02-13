987653ec717ccf8c317bfc67e3f1db5976e2bff6
William Mantly <wmantly@gmail.com>
2019-02-13 17:29:51 -0500

    testing

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const requestIp = require('request-ip');
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/text', function(req, res){
+	console.log(requestIp.getClientIp(req));
+	res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
