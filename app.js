44eb5a3781cbcc76742ddf11b6e422d79544582e
William Mantly <wmantly@gmail.com>
2019-02-13 23:19:06 -0500

    packages

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const requestIp = require('request-ip');
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
 app.get('/text', function(req, res){
 	console.log(requestIp.getClientIp(req));
-	res.send('Hello World!');
+	res.send(requestIp.getClientIp(req));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
